8431371a51e58ad5969204ffad47426bbc323453
Buratti <alessio.buratti95@icloud.com>
2014-10-13 20:20:12 +0200

    pronte le basi e la stampa di figure singole, da implementare il controllo se i pezzi in verticale sono nascosti o meno

--- lego_piece.py
+++ lego_piece.py
@@ -40,6 +40,7 @@ def lego_piece(width, height, color, unit = unit): #draws the base of the lego p
 
     turtle.penup()
     turtle.home()
+    turtle.pensize(1)
     turtle.pendown()
 
 def goTo(x, y, unit = unit):
@@ -52,5 +53,91 @@ def goTo(x, y, unit = unit):
 
 ########################### VERTICAL VIEW
 
+def bordo(height = unit+unit):
+    turtle.left(90)
+    turtle.forward(height)
+    turtle.backward(height)
+    turtle.right(90)
+
+def dot_vertical(height = unit+unit, unit = unit): #funzione cazzona
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(height)
+    turtle.right(90)
+    turtle.forward(unit/4)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.left(90)
+    turtle.forward(unit/4)
+    turtle.right(90)
+    turtle.forward(unit/2)
+    turtle.right(90)
+    turtle.forward(unit/4)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.right(90)
+    turtle.forward(3*unit//4)
+    turtle.left(90)
+    turtle.forward(height)
+    turtle.left(90)
+    turtle.pendown()
+
+def pezzo_singolo(colore_fill, colore_bordo, unit = unit, height = unit+unit):
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.pencolor(colore_bordo)
+        turtle.forward(unit)
+        turtle.left(90)
+        turtle.pencolor(colore_fill)
+        turtle.forward(height)
+        turtle.left(90)
+    turtle.end_fill()
+    turtle.pencolor(colore_bordo)
+    dot_vertical(height)
+
+def check_z():
+    return True
+
+def pezzo_verticale(width, color, posizione, unit = unit):
+    colors = {'red': '#7F100C', 'yellow': '#CCA210', 'white':'#C7C7CC', 'orange': '#CC7323', 'blue': '#1D1B7F', 'black': '#292827'}
+    turtle.pencolor(colors[color])
+    turtle.fillcolor(color)
+    turtle.penup()
+    turtle.forward(posizione*unit)
+    turtle.pendown()
+    for i in range(width):
+        if check_z():
+            pezzo_singolo(color, colors[color])
+        turtle.forward(unit)
+    bordo()
+    turtle.backward(width*unit)
+    bordo()
 
 
+
+
+
+
+
+
+
+
+
+########## Base verticale
+
+def disegna_base(unit = unit, width = 8):
+    lego_piece(width,width,'blue')
+    turtle.penup()
+    turtle.backward(250)
+    turtle.pendown()
+    turtle.pencolor('#1D1B7F')
+    turtle.fillcolor('blue')
+    for i in range(width):
+        pezzo_singolo('blue', '#1D1B7F', unit, unit/2)
+        turtle.forward(unit)
+    bordo(unit/2)
+    turtle.backward(width*unit)
+    bordo(unit/2)
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
--- main.py
+++ main.py
@@ -7,4 +7,16 @@ from lego_piece import *
 # goTo(9,1)
 # lego_piece(1,4,'white')
 # lego_piece(1,8,'yellow')
-vertical_lego_piece(4, 'blue')
+
+disegna_base()
+
+def pezzo(width, height, color, x, y):
+    goTo(x, y)
+    lego_piece(width, height, color)
+    turtle.penup()
+    turtle.goto(-250,unit/2)
+    turtle.pendown()
+    pezzo_verticale(width, color, x)
+
+pezzo(2,3, 'red', 2,3)
+pezzo(1,1, 'orange', 7,1)
